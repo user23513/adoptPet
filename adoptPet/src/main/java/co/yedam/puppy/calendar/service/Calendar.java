a936fb2959e71967f4afb152a375d250f5d4a67c
admin <admin@DESKTOP-M646DHR>
2022-07-14 17:46:58 +0900

    Calendar() ~ing

--- adoptPet/src/main/java/co/yedam/puppy/calendar/service/Calendar.java
+++ adoptPet/src/main/java/co/yedam/puppy/calendar/service/Calendar.java
@@ -1,5 +1,7 @@
 package co.yedam.puppy.calendar.service;
 
+import java.sql.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,6 +9,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import co.yedam.puppy.comm.Command;
+import co.yedam.puppy.vo.CalendarVO;
 
 public class Calendar implements Command {
 
@@ -14,6 +17,15 @@ public class Calendar implements Command {
 	public String exec(HttpServletRequest request, HttpServletResponse response) {
 		// 캘린더
 		CalendarService dao = new CalendarServiceImpl(); // 인스턴스 생성
+		CalendarVO vo = new CalendarVO();
+		String calendarNo = request.getParameter("calendarNo");
+		vo.setCalendarNo(Integer.parseInt(calendarNo));
+		vo.setCalendarWriter(request.getParameter("calendarWriter"));
+		vo.setCalendarTitle(request.getParameter("calendarTitle"));
+		String startDate = request.getParameter("startDate");
+		vo.setCalendarStartDate(Date.valueOf(startDate));
+		String endDate = request.getParameter("endDate");
+		vo.setCalendarEndDate(Date.valueOf("endDate"));
 		Gson gson = new GsonBuilder().create();
 		
 		return "calendar/calendar";
