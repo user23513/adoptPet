d586e05018b3670cf56f505619c6197190e7cc63
admin <admin@DESKTOP-M646DHR>
2022-07-14 10:47:21 +0900

    박은지

--- adoptPet/src/main/java/co/yedam/puppy/calendar/vo/vo.java
+++ adoptPet/src/main/java/co/yedam/puppy/calendar/vo/vo.java
@@ -1,5 +1,18 @@
 package co.yedam.puppy.calendar.vo;
 
-public class vo {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class vo {
+	
+	// 필드
+	private int calendarNo;
+	private String calendarWriter;
+	private String calendarTitle;
+	private String calendarStartDate;
+	private String calendarEndDate;
+	
+	
 }
--- adoptPet/src/main/java/co/yedam/puppy/volunteerSubscription/vo/vo.java
+++ adoptPet/src/main/java/co/yedam/puppy/volunteerSubscription/vo/vo.java
@@ -1,5 +1,17 @@
 package co.yedam.puppy.volunteerSubscription.vo;
 
-public class vo {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+
+public class vo {
+	
+	// 필드
+	private String memberId;
+	private int calendarNo;
+	private String volunteerSubscriptionOk;
+	
+	
 }
