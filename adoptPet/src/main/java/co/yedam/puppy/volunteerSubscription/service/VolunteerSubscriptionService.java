6009377d587fb5914f2a271e72a10a222349e9b4
admin <admin@DESKTOP-M646DHR>
2022-07-14 14:31:08 +0900

    은지 봉사활동 추가

--- adoptPet/src/main/java/co/yedam/puppy/volunteerSubscription/service/VolunteerSubscriptionService.java
+++ adoptPet/src/main/java/co/yedam/puppy/volunteerSubscription/service/VolunteerSubscriptionService.java
@@ -6,9 +6,10 @@ import co.yedam.puppy.adoptSubscription.vo.AdoptSubscriptionVO;
 import co.yedam.puppy.petList.vo.PetListVO;
 
 public interface VolunteerSubscriptionService {
-	//경아
+	//경아(은지)
 	//회원마이페이지
 	List<PetListVO> myVolunteerList(int startRow, int pageSize); // 나의 봉사신청 리스트
+	int myVolunteerInsert(AdoptSubscriptionVO vo); // 나의 봉사신청 추가
 	int myVolunteerUpdate(AdoptSubscriptionVO vo);// 나의 봉사신청 수정
 	int myVolunteerDelete(AdoptSubscriptionVO vo); // 나의 봉사신청 삭제
 
