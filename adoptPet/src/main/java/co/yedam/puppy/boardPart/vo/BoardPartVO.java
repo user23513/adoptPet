f5003040cb9671b6ad77721ba432df7fee5e69d6
admin <admin@DESKTOP-BJ6C3LP>
2022-07-14 10:45:34 +0900

    경아VO

---
+++ adoptPet/src/main/java/co/yedam/puppy/boardPart/vo/BoardPartVO.java
@@ -0,0 +1,13 @@
+package co.yedam.puppy.boardPart.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardPartVO {
+	private int boardId;
+	private String boardName;
+	
+	//머지테스트테스트
+}
---
+++ adoptPet/src/main/java/co/yedam/puppy/volunteerSubscription/vo/VolunteerSubscriptionVO.java
@@ -0,0 +1,12 @@
+package co.yedam.puppy.volunteerSubscription.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VolunteerSubscriptionVO {
+	private String memberId;
+	private int calendarNo;
+	private String volunteerSubscriptionOk;
+}
