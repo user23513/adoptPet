b63128edd71d679e4df22c2fb5151d4100c447af
soom5 <soom5@gmail.com>
2022-07-18 17:27:01 +0900

    20220718 동물소개게시판하는중

--- adoptPet/src/main/java/co/yedam/puppy/files/service/FilesService.java
+++ adoptPet/src/main/java/co/yedam/puppy/files/service/FilesService.java
@@ -1,9 +1,14 @@
 package co.yedam.puppy.files.service;
 
+import java.util.List;
+
 import co.yedam.puppy.vo.FilesVO;
+import co.yedam.puppy.vo.PetListVO;
 
 public interface FilesService {
 	
 	int filesInsert(FilesVO vo); //첨부파일 등록
-
+	List<FilesVO> fileNoSelect(int petListNo); //선택한 게시글에 파일넘버 가지고오기
+	int filesDelete(int filesNo); //파일삭제
+	List<String> filesSelect(PetListVO vo); //파일조회
 }
