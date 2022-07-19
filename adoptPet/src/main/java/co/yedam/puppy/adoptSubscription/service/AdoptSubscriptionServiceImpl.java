05d903eaa13fb71fd6e3ebd4bf96039715fea04a
admin <admin@DESKTOP-0HTCQKS>
2022-07-19 18:00:59 +0900

    220719 학원_머지 후 다시 커밋

--- adoptPet/src/main/java/co/yedam/puppy/adoptSubscription/service/AdoptSubscriptionServiceImpl.java
+++ adoptPet/src/main/java/co/yedam/puppy/adoptSubscription/service/AdoptSubscriptionServiceImpl.java
@@ -45,12 +45,6 @@ public class AdoptSubscriptionServiceImpl implements AdoptSubscriptionService {
 		return list;
 	}
 
-	@Override
-	public int AdoptSubInsert(AdoptSubscriptionVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
-
-
 
 	@Override
 	public int AdoptSubInsert(AdoptSubscriptionVO vo) {
